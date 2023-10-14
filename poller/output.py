4911ef44c95e2ab92e5de1b85653dcfce43e951f
Rich <richard.a.cairns@gmail.com>
2023-10-14 12:48:19 +0100

    refactor to put template file in the same dir as the code callling it

---
+++ poller/output.py
@@ -0,0 +1,28 @@
+"""
+Create a formatted text output that can be easily pasted into a Discourse post to create a poll.
+"""
+from jinja2 import Environment, FileSystemLoader
+from poller import config
+
+
+class Output:
+    """
+    Creates formatted text that can be copy-pasted directly into Discourse with no further formatting to create a voting poll.
+    """
+
+    def __init__(self, entrants, voting_deadline):
+        self.entrants = entrants
+        self.voting_deadline = voting_deadline
+        self.environment = Environment(loader=FileSystemLoader("."))
+        self.template = self.environment.get_template("template.txt")
+
+    def render(self):
+        return self.template.render(
+            entrants=self.entrants, voting_deadline=self.voting_deadline
+        )
+
+
+if __name__ == "__main__":
+    output = Output(entrants=["Person1", "Person2"], voting_deadline="1/1/2024")
+
+    print(output.render())
