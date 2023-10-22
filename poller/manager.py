8c87580b63771b7e0c06c53cb308bddaab7618d8
Rich <richard.a.cairns@gmail.com>
2023-10-22 14:46:55 +0100

    Add comments

--- poller/manager.py
+++ poller/manager.py
@@ -1,8 +1,17 @@
+"""
+The main interface for other code to call poller.
+"""
 from .discourse_thread import DiscourseThread
 from .output import Output
 
 
 class Manager:
+    """
+    An instance of the Manager class is the interface for other code to work with poller. The class orchestrates the
+    retrieval and parsing of the thread and the production of the rendered output, and abstracts the logic away from
+    the calling code.
+    """
+
     def __init__(self, topic_id, voting_deadline):
         self.topic_id = topic_id
         self.voting_deadline = voting_deadline
@@ -10,9 +19,16 @@ class Manager:
         self.output = self._get_rendered_output()
 
     def _get_entrants(self):
+        """
+        Create a list of posters in a thread.
+        """
         poll = DiscourseThread(self.topic_id)
         return poll.all_entrants
 
     def _get_rendered_output(self):
+        """
+        Render the output, merging the list of posters with a template to produce text that can be pasted into a Discourse
+        thread to display a poll.
+        """
         output = Output(self.entrants, self.voting_deadline)
         return output.render()
